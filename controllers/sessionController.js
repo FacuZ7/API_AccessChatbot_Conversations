7ab67cbc27c7c2d3bab1651d986fec030ef065b1
fernandounias <44074668+fernandounias@users.noreply.github.com>
2024-06-16 00:02:36 -0300

    changes to sessionController

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,4 +1,4 @@
-import { SessionModelModel } from "../models/schemaIndex";
+import { SessionModel } from "../models/schemaIndex";
 import handleHTTPError from "../utils/handleError";
 
 const getSessionById = async (req, res) => {
@@ -14,8 +14,30 @@ const getSessionById = async (req, res) => {
 
 const createSession = async (req, res) => {
     //POST /session/:id
+    try {
+        const { body } = req;
+        const data = await SessionModel.create(body);
+        res.send({ data });
+    } catch (error) {
+        handleHTTPError(res, "CREATE_SESSION_ERROR");
+    }
 }
 
 const updateSession = async (req, res) => {
     //PATCH /session/:id
+    try {
+        const { id } = req.params;
+        const { body } = req;
+        const data = await SessionModel.findByIdAndUpdate(id, body, { new: true }, function(err, doc) {
+            if(err){
+                handleHTTPError(res, "UPDATE_ITEM_ERROR", 304);        
+            }
+            // else{    res.send({doc})    }
+        });
+        res.send({data});
+    } catch (error) {
+        handleHTTPError(res, "UPDATE_ITEM_ERROR");
+    }
 }
+
+export { getSessionById, createSession, updateSession };
