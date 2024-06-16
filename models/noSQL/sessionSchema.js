29ccf790db7f2c7a2fb40beefbfa9129c97b0a33
fernandounias <44074668+fernandounias@users.noreply.github.com>
2024-06-15 23:18:54 -0300

    adding sessionSchema

---
+++ models/noSQL/sessionSchema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema({
+    sessionId: {
+        type: String,
+        unique: true,
+        required: true,
+      },
+    chatId: {
+        type: String,
+        required: true,
+      },
+    fecha: {
+        type: Date,
+        default: Date.now,
+        required: true,
+      },
+}, {
+      timestamps: true, // This will add `createdAt` and `updatedAt` fields
+}
+);
+
+const Session = mongoose.model("Session", sessionSchema);
+
+export default Session;
