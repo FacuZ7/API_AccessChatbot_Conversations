1caf971ec4c938e9fe442c29d3e7f2fa3771773c
marsleiman <sleimanmariamn@gmail.com>
2024-06-22 19:31:52 -0300

    swagger doc

--- app.js
+++ app.js
@@ -3,11 +3,35 @@ import express from "express";
 import cors from "cors";
 import dbConnect from "./config/mongoDB.js";
 import routerMaster from "./routes/routerMaster.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 
 const app = express();
 dotenv.config();
 
-//Importante el CORS PRIMERO este es el orden correcto.
+// Configuración de opciones para swagger-jsdoc
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Mi API',
+      version: '1.0.0',
+      description: 'Documentación de mi API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8080', // URL base de tu API
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Rutas donde están tus archivos de rutas
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+// Ruta para servir la documentación de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(cors());
 app.use(express.json()); //esta linea permite recibir mediante los body, información en formato JSON
 app.use("/", routerMaster);
--- routes/clientRouter.js
+++ routes/clientRouter.js
@@ -9,6 +9,37 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /conversations/:
+ *   get:
+ *     summary: Obtiene todos los usuarios
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
 router.get("/", getAllItems);
 router.get("/:id", getItemById);
 router.post("/", createItem);
