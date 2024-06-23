34793bb2ecb05b85e78bd3e1423909380144aa03
marsleiman <sleimanmariamn@gmail.com>
2024-06-23 00:06:17 -0300

    realted_vector_id array string id

--- controllers/vectorController.js
+++ controllers/vectorController.js
@@ -4,18 +4,19 @@ import handleHTTPError from "../utils/handleError.js";
 const createVector = async (req, res) => {
     const vectorId = req.body.vector_id;
     const fileName = req.body.filename;
+    const arrayVectorsId = req.body.realted_vector_id;
   
     const vector = new VectorSchema({
       vector_id: vectorId,
       filename: fileName,
-      realted_vector_id: [],
+      realted_vector_id: arrayVectorsId,
     });
   
     try {
       const newVector = await vector.save();
       res.json(newVector);
     } catch (error) {
-      handleHTTPError(res, "CREATE_ITEM_ERROR"); //por default devuelve codigo 403
+      handleHTTPError(res, "CREATE_VECTOR_ERROR");
     }
 };
 
